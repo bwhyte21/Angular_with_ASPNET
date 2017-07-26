d98b51a250ca441106f80a05907df55bc3448e4e
bwhyte <bwhyte@MEDLAPTOP1072>
2017-07-26 12:54:32 -0400

    inital commit

---
+++ AngularJS_with_Net/AngularJS_with_Net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularJS_with_Net.Startup))]
+namespace AngularJS_with_Net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
