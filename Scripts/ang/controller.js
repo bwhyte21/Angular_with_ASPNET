4c24eb161c0d5edb4461c225c4cc8fd20f5351d3
Bryan Whyte <bwhyte21@outlook.com>
2017-07-27 02:55:12 -0400

    udemy class completed
    Had to physically update because of 403 error from home computer

---
+++ Scripts/ang/controller.js
@@ -0,0 +1,9 @@
+﻿var myApp = angular.module('myApp', []);
+
+//$scope is like a viewbag in code behind
+myApp.controller('myAppCtrl', function ($scope) {
+    $scope.somestring = 'test string';
+
+    // make angular employee model
+    $scope.angEmp = Model;
+});
